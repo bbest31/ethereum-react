204695539cc76dc77371ad9e52f5ca04cdbce28e
bbest <brandon@airmatrix.ca>
2021-04-11 14:30:54 -0400

    First video done

--- README.md
+++ README.md
@@ -68,3 +68,14 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+### Blockchain Dependencies
+
+#### web3.js
+
+Collection of JavaScript libraries which allow you to interact with a local or remote ethereum node, using HTTP or IPC connection.
+
+```
+npm install --save web3
+```
--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Web3 from "web3";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: null,
+    };
+  }
+
+  componentDidMount() {
+    this.loadBlockchainData();
+  }
+
+  /**
+   * loadBlockchainData asynchronously retrieves
+   */
+  loadBlockchainData = async () => {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    const network = await web3.eth.net.getNetworkType();
+    console.log("network:", network);
+    // Fetch account
+    // must first enable/connect them from the current env
+    const requestedAccounts = await web3.eth.requestAccounts();
+    console.log(requestedAccounts);
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+    this.setState({
+      account: accounts[0],
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1>ETH + React Todo List</h1>
+        <p>Your account: {this.state.account}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
